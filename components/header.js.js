faa4197485501a4829651fb96a9ebb3fdc2e141a
nahuumaidana <nmaidana247@gmail.com>
2025-07-06 15:30:08 -0300

    forzar redeploy

--- components/header.js.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header style={{ padding: '1rem', backgroundColor: '#111', color: 'orange', fontWeight: 'bold' }}>
-      <h1>ARGENSIMIOS 🦍</h1>
-    </header>
-  )
-}
